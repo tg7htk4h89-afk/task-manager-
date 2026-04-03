e8d7710e0bb41a48d7b906309db7f43294ea3cb3
tg7htk4h89-afk <tg7htk4h89@privaterelay.appleid.com>
2026-04-04 00:06:37 +0300

    Delete assets directory

--- assets/js/sample-loader.js
+++
@@ -1,7 +0,0 @@
-
-window.SampleStore = {
-  async load() {
-    const response = await fetch("../assets/data/sample-data.json".replace("..",".")); // fallback for root page
-    return response.json();
-  }
-};
